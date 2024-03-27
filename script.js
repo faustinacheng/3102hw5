26b750839a947be7be77fde56a75c861d87a89a1
Faustina <83921854+faustinacheng@users.noreply.github.com>
2024-03-27 15:45:10 -0400

    Finished hw5

---
+++ script.js
@@ -0,0 +1,17 @@
+fetch("https://randomuser.me/api/?results=5")
+    .then((data) => {
+        return data.json();
+    })
+    .then((objectData) => {
+        let tableData = "";
+        objectData.results.map((values) => {
+            tableData += `<tr>
+            <td>${values.name.first}</td>
+            <td>${values.location.city}</td>
+            </tr>`;
+        });
+        document.getElementById("table-body").innerHTML = tableData;
+    })
+    .catch((error) => {
+        console.log(error);
+    });
